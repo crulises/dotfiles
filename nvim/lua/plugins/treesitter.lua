15a474a413af28dd62f7a10bea3096b0ee560af4
Uli <ulisesrcolina@gmail.com>
2026-01-05 04:31:11 +0000

    fix: [treesitter + telescope] tag correcto para funcionar con nvim v0.10

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,26 +5,13 @@
 
 return {
   'nvim-treesitter/nvim-treesitter',
+  tag = 'v0.10.0',
   build = ':TSUpdate',
   event = { 'BufReadPost', 'BufNewFile' },
 
   main = 'nvim-treesitter.configs',
   opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'html',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'python',
-      'vim',
-      'vimdoc',
-      'yaml',
-      'json',
-    },
+    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'vim', 'vimdoc', 'yaml', 'json', },
     auto_install = true,
     highlight = { enable = true },
     indent = { enable = true },
